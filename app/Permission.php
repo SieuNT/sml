12bf8d1138ac3e56e343f76dc56dfaa9212d6e97
NguyenTuanSieu <dev.phpjs@gmail.com>
2017-03-21 02:50:15 +0700

    .

--- app/Permission.php
+++ app/Permission.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
-    //
+    /**
+     * @var string
+     */
+    protected $table = 'permissions';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['name', 'slug', 'description'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function roles() {
+        return $this->belongsToMany(Role::class);
+    }
 }
--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    /**
+     * @var string
+     */
+    protected $table = 'roles';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['name', 'slug'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users() {
+        return $this->belongsToMany(User::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function permissions() {
+        return $this->belongsToMany(Permission::class)->withTimestamps();
+    }
 }
