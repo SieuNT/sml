3af9c0bec71db72dca55c2ced38f14b644947193
NguyenTuanSieu <dev.phpjs@gmail.com>
2017-03-21 04:05:40 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,38 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'Frontend'], function() {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    // Authentication Routes...
+    Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+    Route::post('login', 'Auth\LoginController@login');
+    Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+    // Registration Routes...
+    Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'Auth\RegisterController@register');
+    // Password Reset Routes...
+    Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+    //Frontend router
+    Route::get('/home', 'HomeController@index');
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index');
+Route::group(['prefix' => 'admin', 'namespace' => 'Backend'], function() {
+    Route::get('/', 'DashboardController@index');
+    // Authentication Routes...
+    Route::get('login', 'Auth\LoginController@showLoginForm')->name('backend.login');
+    Route::post('login', 'Auth\LoginController@login');
+    Route::post('logout', 'Auth\LoginController@logout')->name('backend.logout');
+    // Registration Routes...
+    Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('backend.register');
+    Route::post('register', 'Auth\RegisterController@register');
+    // Password Reset Routes...
+    Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('backend.password.request');
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('backend.password.email');
+    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('backend.password.reset');
+    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+});
