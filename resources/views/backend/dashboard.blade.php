83ba9124ca97c74398eec852955aaebd63fd1d0d
NguyenTuanSieu <dev.phpjs@gmail.com>
2017-03-28 00:26:31 +0700

    .

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -1,17 +1,91 @@
 @extends('layouts.backend')
 
 @section('content')
-<div class="container">
+    <div class="page-title">
+        <h3 class="title">Blank Page</h3>
+    </div>
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    [Admin] You are logged in!
-                </div>
+        <div class="panel panel-purple">
+            <div class="panel-body">
+                <table class="table table-bordered table-striped">
+                    <thead>
+                    <tr>
+                        <th>Company</th>
+                        <th>Last Trade</th>
+                        <th>Trade Time</th>
+                        <th>Change</th>
+                        <th>Prev Close</th>
+                        <th>Open</th>
+                        <th>Bid</th>
+                        <th>Ask</th>
+                        <th>1y Target Est</th>
+                        <th>Lorem</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th>GOOG <span class="co-name">Google Inc.</span></th>
+                        <td>597.74</td>
+                        <td>12:12PM</td>
+                        <td>14.81 (2.54%)</td>
+                        <td>582.93</td>
+                        <td>597.95</td>
+                        <td>597.73 x 100</td>
+                        <td>597.91 x 300</td>
+                        <td>731.10</td>
+                        <td>Spanning cell</td>
+                    </tr>
+                    <tr>
+                        <th>GOOG <span class="co-name">Google Inc.</span></th>
+                        <td>597.74</td>
+                        <td>12:12PM</td>
+                        <td>14.81 (2.54%)</td>
+                        <td>582.93</td>
+                        <td>597.95</td>
+                        <td>597.73 x 100</td>
+                        <td>597.91 x 300</td>
+                        <td>731.10</td>
+                        <td>Spanning cell</td>
+                    </tr>
+                    <tr>
+                        <th>GOOG <span class="co-name">Google Inc.</span></th>
+                        <td>597.74</td>
+                        <td>12:12PM</td>
+                        <td>14.81 (2.54%)</td>
+                        <td>582.93</td>
+                        <td>597.95</td>
+                        <td>597.73 x 100</td>
+                        <td>597.91 x 300</td>
+                        <td>731.10</td>
+                        <td>Spanning cell</td>
+                    </tr>
+                    <tr>
+                        <th>GOOG <span class="co-name">Google Inc.</span></th>
+                        <td>597.74</td>
+                        <td>12:12PM</td>
+                        <td>14.81 (2.54%)</td>
+                        <td>582.93</td>
+                        <td>597.95</td>
+                        <td>597.73 x 100</td>
+                        <td>597.91 x 300</td>
+                        <td>731.10</td>
+                        <td>Spanning cell</td>
+                    </tr>
+                    <tr>
+                        <th>GOOG <span class="co-name">Google Inc.</span></th>
+                        <td>597.74</td>
+                        <td>12:12PM</td>
+                        <td>14.81 (2.54%)</td>
+                        <td>582.93</td>
+                        <td>597.95</td>
+                        <td>597.73 x 100</td>
+                        <td>597.91 x 300</td>
+                        <td>731.10</td>
+                        <td>Spanning cell</td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -121,106 +121,13 @@
                         </ul>
                     @endif
                 </li>
-                <!-- user login dropdown end -->
             </ul>
-            <!-- End right navbar -->
         </nav>
 
     </header>
-    <!-- Header Ends -->
-    <!-- Page Content Start -->
-    <!-- ================== -->
 
     <div class="wraper container-fluid">
-        <div class="page-title">
-            <h3 class="title">Blank Page</h3>
-        </div>
-        <div class="row">
-            <div class="panel panel-purple">
-                <div class="panel-body">
-                    <table class="table table-bordered table-striped">
-                        <thead>
-                        <tr>
-                            <th>Company</th>
-                            <th>Last Trade</th>
-                            <th>Trade Time</th>
-                            <th>Change</th>
-                            <th>Prev Close</th>
-                            <th>Open</th>
-                            <th>Bid</th>
-                            <th>Ask</th>
-                            <th>1y Target Est</th>
-                            <th>Lorem</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <th>GOOG <span class="co-name">Google Inc.</span></th>
-                            <td>597.74</td>
-                            <td>12:12PM</td>
-                            <td>14.81 (2.54%)</td>
-                            <td>582.93</td>
-                            <td>597.95</td>
-                            <td>597.73 x 100</td>
-                            <td>597.91 x 300</td>
-                            <td>731.10</td>
-                            <td>Spanning cell</td>
-                        </tr>
-                        <tr>
-                            <th>GOOG <span class="co-name">Google Inc.</span></th>
-                            <td>597.74</td>
-                            <td>12:12PM</td>
-                            <td>14.81 (2.54%)</td>
-                            <td>582.93</td>
-                            <td>597.95</td>
-                            <td>597.73 x 100</td>
-                            <td>597.91 x 300</td>
-                            <td>731.10</td>
-                            <td>Spanning cell</td>
-                        </tr>
-                        <tr>
-                            <th>GOOG <span class="co-name">Google Inc.</span></th>
-                            <td>597.74</td>
-                            <td>12:12PM</td>
-                            <td>14.81 (2.54%)</td>
-                            <td>582.93</td>
-                            <td>597.95</td>
-                            <td>597.73 x 100</td>
-                            <td>597.91 x 300</td>
-                            <td>731.10</td>
-                            <td>Spanning cell</td>
-                        </tr>
-                        <tr>
-                            <th>GOOG <span class="co-name">Google Inc.</span></th>
-                            <td>597.74</td>
-                            <td>12:12PM</td>
-                            <td>14.81 (2.54%)</td>
-                            <td>582.93</td>
-                            <td>597.95</td>
-                            <td>597.73 x 100</td>
-                            <td>597.91 x 300</td>
-                            <td>731.10</td>
-                            <td>Spanning cell</td>
-                        </tr>
-                        <tr>
-                            <th>GOOG <span class="co-name">Google Inc.</span></th>
-                            <td>597.74</td>
-                            <td>12:12PM</td>
-                            <td>14.81 (2.54%)</td>
-                            <td>582.93</td>
-                            <td>597.95</td>
-                            <td>597.73 x 100</td>
-                            <td>597.91 x 300</td>
-                            <td>731.10</td>
-                            <td>Spanning cell</td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-
-
+        @yield('content')
     </div>
     <!-- Page Content Ends -->
     <!-- Footer Start -->
