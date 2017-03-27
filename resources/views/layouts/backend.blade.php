494b1d11ae755163e734bbf00f973bcb5792c3b5
NguyenTuanSieu <dev.phpjs@gmail.com>
2017-03-28 00:24:55 +0700

    .

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -35,8 +35,8 @@
     <!-- brand -->
     <div class="logo">
         <a href="index.html" class="logo-expanded">
-            <i class="ion-social-buffer"></i>
-            <span class="nav-label">Velonic</span>
+            <i class="ion-ionic"></i>
+            <span class="nav-label">VinPearl</span>
         </a>
     </div>
     <!-- / brand -->
@@ -45,97 +45,41 @@
         <ul class="list-unstyled">
             <li class="active"><a href="{{route('backend')}}"><i class="ion-home"></i> <span
                             class="nav-label">Bảng điều khiển</span></a></li>
-            <li class="has-submenu"><a href="#"><i class="ion-android-apps"></i> <span
-                            class="nav-label">Tin tức</span></a>
-                <ul class="list-unstyled">
-                    <li><a href="typography.html">Typography</a></li>
-                    <li><a href="buttons.html">Buttons</a></li>
-                    <li><a href="icons.html">Icons</a></li>
-                    <li><a href="panels.html">Panels</a></li>
-                    <li><a href="tabs-accordions.html">Tabs &amp; Accordions</a></li>
-                    <li><a href="modals.html">Modals</a></li>
-                    <li><a href="bootstrap-ui.html">BS Elements</a></li>
-                    <li><a href="progressbars.html">Progress Bars</a></li>
-                    <li><a href="notification.html">Notification</a></li>
-                    <li><a href="sweet-alert.html">Sweet-Alert</a></li>
-                </ul>
+            <li class="has-submenu">
+                <a href="#">
+                    <i class="ion-android-apps"></i>
+                    <span class="nav-label">Tin tức</span>
+                </a>
             </li>
-            <li class="has-submenu"><a href="#"><i class="ion-settings"></i> <span
-                            class="nav-label">Components</span><span class="badge bg-success">New</span></a>
-                <ul class="list-unstyled">
-                    <li><a href="grid.html">Grid</a></li>
-                    <li><a href="portlets.html">Portlets</a></li>
-                    <li><a href="widgets.html">Widgets</a></li>
-                    <li><a href="nestable-list.html">Nesteble</a></li>
-                    <li><a href="calendar.html">Calendar</a></li>
-                    <li><a href="ui-sliders.html">Range Slider</a></li>
-                </ul>
+            <li class="has-submenu">
+                <a href="#">
+                    <i class="ion-settings"></i>
+                    <span class="nav-label">Câu chuyên khách hàng</span>
+                </a>
             </li>
-            <li class="has-submenu"><a href="#"><i class="ion-compose"></i> <span class="nav-label">Forms</span></a>
-                <ul class="list-unstyled">
-                    <li><a href="form-elements.html">General Elements</a></li>
-                    <li><a href="form-validation.html">Form Validation</a></li>
-                    <li><a href="form-advanced.html">Advanced Form</a></li>
-                    <li><a href="form-wizard.html">Form Wizard</a></li>
-                    <li><a href="form-editor.html">WYSIWYG Editor</a></li>
-                    <li><a href="code-editor.html">Code Editors</a></li>
-                    <li><a href="form-uploads.html">Multiple File Upload</a></li>
-                    <li><a href="image-crop.html">Image Crop</a></li>
-                    <li><a href="form-xeditable.html">X-Editable</a></li>
-                </ul>
+            <li class="has-submenu">
+                <a href="#"><i class="ion-compose"></i>
+                    <span class="nav-label">Dự án tiêu biểu</span>
+                </a>
             </li>
-            <li class="has-submenu"><a href="#"><i class="ion-grid"></i> <span class="nav-label">Data Tables</span></a>
-                <ul class="list-unstyled">
-                    <li><a href="tables.html">Basic Tables</a></li>
-                    <li><a href="table-datatable.html">Data Table</a></li>
-                    <li><a href="tables-editable.html">Editable Table</a></li>
-                    <li><a href="responsive-table.html">Responsive Table</a></li>
-                </ul>
+            <li class="has-submenu">
+                <a href="#">
+                    <i class="ion-grid"></i>
+                    <span class="nav-label">Câu hỏi thường gặp</span>
+                </a>
             </li>
-            <li class="has-submenu"><a href="#"><i class="ion-stats-bars"></i> <span
-                            class="nav-label">Charts</span><span class="badge bg-purple">1</span></a>
-                <ul class="list-unstyled">
-                    <li><a href="morris-chart.html">Morris Chart</a></li>
-                    <li><a href="chartjs.html">Chartjs</a></li>
-                    <li><a href="flot-chart.html">Flot Chart</a></li>
-                    <li><a href="rickshaw-chart.html">Rickshaw Chart</a></li>
-                    <li><a href="peity-chart.html">Peity Chart</a></li>
-                    <li><a href="c3-chart.html">C3 Chart</a></li>
-                    <li><a href="other-chart.html">Other Chart</a></li>
-                </ul>
+            <li class="has-submenu">
+                <a href="#">
+                    <i class="ion-stats-bars"></i>
+                    <span class="nav-label">Video</span>
+                </a>
             </li>
 
-            <li class="has-submenu"><a href="#"><i class="ion-email"></i> <span class="nav-label">Mail</span></a>
-                <ul class="list-unstyled">
-                    <li><a href="inbox.html">Inbox</a></li>
-                    <li><a href="email-compose.html">Compose Mail</a></li>
-                    <li><a href="email-read.html">View Mail</a></li>
-                    <li><a href="email-templates.html">Email Templates</a></li>
-                </ul>
-            </li>
-
-            <li class="has-submenu"><a href="#"><i class="ion-location"></i> <span class="nav-label">Maps</span></a>
-                <ul class="list-unstyled">
-                    <li><a href="gmap.html"> Google Map</a></li>
-                    <li><a href="vector-map.html"> Vector Map</a></li>
-                </ul>
-            </li>
-            <li class="has-submenu"><a href="#"><i class="ion-document"></i> <span class="nav-label">Pages</span><span
-                            class="badge bg-pink">5</span></a>
-                <ul class="list-unstyled">
-                    <li><a href="profile.html">Profile</a></li>
-                    <li><a href="timeline.html">Timeline</a></li>
-                    <li><a href="invoice.html">Invoice</a></li>
-                    <li><a href="contact.html">Contact-list</a></li>
-                    <li><a href="login.html">Login</a></li>
-                    <li><a href="register.html">Register</a></li>
-                    <li><a href="recoverpw.html">Recover Password</a></li>
-                    <li><a href="lock-screen.html">Lock Screen</a></li>
-                    <li><a href="blank.html">Blank Page</a></li>
-                    <li><a href="404.html">404 Error</a></li>
-                    <li><a href="404_alt.html">404 alt</a></li>
-                    <li><a href="500.html">500 Error</a></li>
-                </ul>
+            <li class="has-submenu">
+                <a href="#">
+                    <i class="ion-email"></i>
+                    <span class="nav-label">Quảng cáo</span>
+                </a>
             </li>
         </ul>
     </nav>
@@ -149,97 +93,9 @@
             <span class="icon-bar"></span>
         </button>
         <!-- Left navbar -->
-        <nav class=" navbar-default" role="navigation">
-            <ul class="nav navbar-nav hidden-xs">
-                <li class="dropdown">
-                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">English <span class="caret"></span></a>
-                    <ul role="menu" class="dropdown-menu">
-                        <li><a href="#">German</a></li>
-                        <li><a href="#">French</a></li>
-                        <li><a href="#">Italian</a></li>
-                        <li><a href="#">Spanish</a></li>
-                    </ul>
-                </li>
-            </ul>
-
+        <nav class="navbar-default" role="navigation">
             <!-- Right navbar -->
             <ul class="nav navbar-nav navbar-right top-menu top-right-menu">
-                <!-- mesages -->
-                <li class="dropdown">
-                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                        <i class="fa fa-envelope-o "></i>
-                        <span class="badge badge-sm up bg-purple count">4</span>
-                    </a>
-                    <ul class="dropdown-menu extended fadeInUp animated nicescroll" tabindex="5001">
-                        <li>
-                            <p>Messages</p>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="pull-left"><img src="{{asset('themes/backend/img/avatar-2.jpg')}}"
-                                                             class="img-circle thumb-sm m-r-15" alt="img"></span>
-                                <span class="block"><strong>John smith</strong></span>
-                                <span class="media-body block">New tasks needs to be done<br><small class="text-muted">10 seconds ago</small></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="pull-left"><img src="{{asset('themes/backend/img/avatar-3.jpg')}}"
-                                                             class="img-circle thumb-sm m-r-15" alt="img"></span>
-                                <span class="block"><strong>John smith</strong></span>
-                                <span class="media-body block">New tasks needs to be done<br><small class="text-muted">3 minutes ago</small></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="pull-left"><img src="{{asset('themes/backend/img/avatar-4.jpg')}}"
-                                                             class="img-circle thumb-sm m-r-15" alt="img"></span>
-                                <span class="block"><strong>John smith</strong></span>
-                                <span class="media-body block">New tasks needs to be done<br><small class="text-muted">10 minutes ago</small></span>
-                            </a>
-                        </li>
-                        <li>
-                            <p><a href="inbox.html" class="text-right">See all Messages</a></p>
-                        </li>
-                    </ul>
-                </li>
-                <!-- /messages -->
-                <!-- Notification -->
-                <li class="dropdown">
-                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                        <i class="fa fa-bell-o"></i>
-                        <span class="badge badge-sm up bg-pink count">3</span>
-                    </a>
-                    <ul class="dropdown-menu extended fadeInUp animated nicescroll" tabindex="5002">
-                        <li class="noti-header">
-                            <p>Notifications</p>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="pull-left"><i class="fa fa-user-plus fa-2x text-info"></i></span>
-                                <span>New user registered<br><small class="text-muted">5 minutes ago</small></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="pull-left"><i class="fa fa-diamond fa-2x text-primary"></i></span>
-                                <span>Use animate.css<br><small class="text-muted">5 minutes ago</small></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="pull-left"><i class="fa fa-bell-o fa-2x text-danger"></i></span>
-                                <span>Send project demo files to client<br><small class="text-muted">1 hour ago</small></span>
-                            </a>
-                        </li>
-
-                        <li>
-                            <p><a href="#" class="text-right">See all notifications</a></p>
-                        </li>
-                    </ul>
-                </li>
-                <!-- /Notification -->
-
                 <!-- user login dropdown start-->
                 <li class="dropdown text-center">
                     @if (Auth::guest())
@@ -252,10 +108,6 @@
                         </a>
                         <ul class="dropdown-menu pro-menu fadeInUp animated" tabindex="5003"
                             style="overflow: hidden; outline: none;">
-                            <li><a href="profile.html"><i class="fa fa-briefcase"></i>Profile</a></li>
-                            <li><a href="#"><i class="fa fa-cog"></i> Settings</a></li>
-                            <li><a href="#"><i class="fa fa-bell"></i> Friends <span
-                                            class="label label-info pull-right mail-info">5</span></a></li>
                             <li>
                                 <a href="{{ route('backend.logout') }}"
                                    onclick="event.preventDefault();
